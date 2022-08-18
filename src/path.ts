7788dff52b2c5652e61f476965e8dfc13e28a259
Yext-Sam-Guo <106765768+Yext-Sam-Guo@users.noreply.github.com>
2022-08-18 10:34:27 -0400

    Update path.ts

--- src/path.ts
+++ src/path.ts
@@ -1,7 +1,7 @@
 import { StreamOutput } from "./data.ts";
 
 export function GetLocationsPath(doc: StreamOutput) {
-  return "location12-" + doc.locale + "-" + doc.id;
+  return "location13-" + doc.locale + "-" + doc.id;
 };
 
 export function GetAboutPath(doc: StreamOutput) {
