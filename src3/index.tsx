5fc886208f6b5a36d30c303195eaffa675cc60d1
Yuhui (Sam) Guo <yguo@yext.com>
2022-08-19 10:41:13 -0400

    reset the testing code

--- src3/index.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactDOM, React } from "./deps.ts";
-import { App } from "./reactApp.tsx";
-
-ReactDOM.hydrate(
-	<App doc={(window as any).__INITIAL__DATA__.doc} />,
-	document.getElementById("root")
-);
--- src3/path.ts
+++
@@ -1,9 +0,0 @@
-import { StreamOutput } from "./data.ts";
-
-export function GetLocationsPath(doc: StreamOutput) {
-  return "location-" + doc.locale + "-" + doc.id;
-};
-
-export function GetAboutPath(doc: StreamOutput) {
-  return "about-" + doc.locale;
-};
